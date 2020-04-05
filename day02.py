3672948ef1c6f74f9e13d4c0170b59a2e029bfdb
Dengchao <930168362@qq.com>
2020-04-05 18:22:35 +0800

    第一次向github提交代码

---
+++ day02.py
@@ -0,0 +1,40 @@
+# '''print("你好")#字符串
+# print(333)#整数
+# print(2.333)#小数
+# print(True)#布尔值 Ture False
+# print(())#元组
+# print([])#数组
+# print({})#字典
+# '''
+
+# '''
+# print('haha',123,2.123)
+# print('哈哈'+'嘻嘻')
+# print('哈哈'*100)
+# print(((1+2)*100)/2)
+# print(2>3)
+# 变量 
+# 赋值
+# a="张三"把张三这个值赋值给了a这个变量
+
+# a = input("请输入；")
+# print("input获取的值；",a)
+# # 数据格式的转换
+# c=type(2333)
+
+
+# print(type("你好"))
+# print(type(333))#整数
+# print(type(2.333))#小数
+# print(type(True))#布尔值 Ture False
+# print(type(()))#元组
+# print(type([]))#数组
+# print(type({}))#字典
+
+
+# a="qxbibxqijxqjxsnjnxqkjnj"
+# print(len(a))
+
+a=input("输入一个值")
+b=input("输入一个值")
+print("长度是；",len(a+b))
